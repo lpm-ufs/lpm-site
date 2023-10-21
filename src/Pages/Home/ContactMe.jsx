975a777a575787cc49f21420ecd5a47cff700c1b
josedhonatas <dhonatas@academico.ufs.br>
2023-10-21 20:16:03 -0300

    jsun

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -1,82 +1,41 @@
+import data from "../../data/index.json";
+
+// Estilo para remover o sublinhado do link de e-mail
+const emailLinkStyle = {
+  textDecoration: "none",
+   // Use a cor de texto padrão
+};
+
 export default function ContactMe() {
   return (
-    <section id="Contact" className="contact--section">
-      <div>
-        <p className="sub--title">Get In Touch</p>
-        <h2>Contact Me</h2>
-        <p className="text-lg">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, odit.
-        </p>
+    <section className="testimonial--section" id="testimonial2">
+      <div className="portfolio--container-box">
+        <div className="portfolio--container">
+          <h2 className="sections--heading">Contato</h2>
+        </div>
       </div>
-      <form className="contact--form--container">
-        <div className="container">
-          <label htmlFor="first-name" className="contact--label">
-            <span className="text-md">First Name</span>
-            <input
-              type="text"
-              className="contact--input text-md"
-              name="first-name"
-              id="first-name"
-              required
-            />
-          </label>
-          <label htmlFor="last-name" className="contact--label">
-            <span className="text-md">Last Name</span>
-            <input
-              type="text"
-              className="contact--input text-md"
-              name="last-name"
-              id="last-name"
-              required
-            />
-          </label>
-          <label htmlFor="email" className="contact--label">
-            <span className="text-md">Email</span>
-            <input
-              type="email"
-              className="contact--input text-md"
-              name="email"
-              id="email"
-              required
-            />
-          </label>
-          <label htmlFor="phone-number" className="contact--label">
-            <span className="text-md">phone-number</span>
-            <input
-              type="number"
-              className="contact--input text-md"
-              name="phone-number"
-              id="phone-number"
-              required
-            />
-          </label>
-        </div>
-        <label htmlFor="choode-topic" className="contact--label">
-          <span className="text-md">Choose a topic</span>
-          <select id="choose-topic" className="contact--input text-md">
-            <option>Select One...</option>
-            <option>Item 1</option>
-            <option>Item 2</option>
-            <option>Item 3</option>
-          </select>
-        </label>
-        <label htmlFor="message" className="contact--label">
-          <span className="text-md">Message</span>
-          <textarea
-            className="contact--input text-md"
-            id="message"
-            rows="8"
-            placeholder="Type your message..."
-          />
-        </label>
-        <label htmlFor="checkboc" className="checkbox--label">
-          <input type="checkbox" required name="checkbox" id="checkbox" />
-          <span className="text-sm">I accept the terms</span>
-        </label>
-        <div>
-          <button className="btn btn-primary contact--form--btn">Submit</button>
-        </div>
-      </form>
+      <div className="portfolio--section--container">
+        {data?.testimonial2?.map((item, index) => (
+          <div key={index} className="testimonial--section--card">
+            <div className="testimonial--section--card--author--detail">
+              <img src={item.src} alt="Avatar" width="105" height="106" />
+              <div>
+                <p className="text-md testimonial--author--name">
+                  {item.author_name}
+                </p>
+
+                {/* Tornando o e-mail clicável sem sublinhado */}
+                <p className="text-md testimonial--author--designation">
+                  <a href={`mailto:${item.email}`} style={emailLinkStyle}>
+                    {item.email}
+                  </a>
+                </p>
+              </div>
+            </div>
+            <p className="text-md">{item.description}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
