1f15381965dd6afd7a2ea07c457921a8f3a92adf
josedhonatas <dhonatas@academico.ufs.br>
2023-10-20 23:11:00 -0300

    mudanca mudancadooo

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,12 +1,10 @@
 export default function AboutMe() {
   return (
     <section id="AboutMe" className="about--section">
-      <div className="about--section--img">
-        <img src="./img/about-me.png" alt="About Me" />
-      </div>
+      <br></br><br></br><br></br>
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
-          <h1 className="skills-section--heading">About Me</h1>
+          <h1 className="skills-section--heading">Sobre</h1>
           <p className="hero--section-description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
             officiis sit debitis omnis harum sed veniam quasi dicta accusamus
