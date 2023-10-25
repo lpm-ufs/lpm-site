b500c9be9514fd114ec55592b525bf4d562ab8ac
josedhonatas <dhonatas@academico.ufs.br>
2023-10-25 16:07:08 -0300

    quase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
+import Navbar2 from "./Pages/Home/Navbar2";
 import Home from "./Pages/Home/Homescreen";
 
 function App() {
--- src/Pages/Home/Testimonials.jsx
+++ src/Pages/Home/Testimonials.jsx
@@ -1,37 +1,43 @@
 import data from "../../data/index.json";
 
 export default function Testimonial() {
-  const filteredStudentCi = data?.student_ci?.filter(item => item.id < 4);
+  const studentCi = data?.student_ci || [];
+  const studentsInGroupsOfThree = [];
+
+  // Dividir os dados em grupos de trêss
+  for (let i = 0; i < studentCi.length; i += 3) {
+    studentsInGroupsOfThree.push(studentCi.slice(i, i + 3));
+  }
 
   return (
     <section className="testimonial--section" id="students">
       <div className="portfolio--container-box">
         <div className="portfolio--container">
-          <h2 className="sections--heading">Alunos Vinculados ao Laboratório</h2>
+          <h4 className="sections--heading">Alunos Vinculados ao Laboratório</h4>
           <p className="sub--title">Alunos de Iniciação Cientifica e Tecnológica</p>
         </div>
       </div>
-      <div className="portfolio--section--container">
-        {filteredStudentCi?.map((item, index) => (
-          <div key={index} className="testimonial--section--card">
-            <div className="testimonial--section--card--review">
-            </div>
-            <div className="testimonial--section--card--author--detail">
-            <img src={item.src} alt="Avatar" width="75" height="76" />
-              <div>
-                <p className="text-md testimonial--author--name">
-                  {item.author_name}
-                </p>
-                <p className="text-md testimonial--author--designation">
-                  {item.author_designation}
-                </p>
+      {studentsInGroupsOfThree.map((group, groupIndex) => (
+        <div key={groupIndex} className="portfolio--section--container">
+          {group.map((item, index) => (
+            <div key={index} className="testimonial--section--card">
+              <div className="testimonial--section--card--review"></div>
+              <div className="testimonial--section--card--author--detail">
+                <img src={item.src} alt="Avatar" width="75" height="76" />
+                <div>
+                  <p className="text-md testimonial--author--name">
+                    {item.author_name}
+                  </p>
+                  <p className="text-md testimonial--author--designation">
+                    {item.author_designation}
+                  </p>
+                </div>
               </div>
+              <p className="text-md">{item.description}</p>
             </div>
-            <p className="text-md">{item.description}</p>
-
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ))}
       <br></br>
       <div className="portfolio--container-box">
         <div className="portfolio--container">
