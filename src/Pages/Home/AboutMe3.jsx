f9835e2752264300e757494cbadf0e61b2815be1
josedhonatas <dhonatas@academico.ufs.br>
2023-10-26 18:12:49 -0300

    correcao

--- src/Pages/Home/AboutMe3.jsx
+++ src/Pages/Home/AboutMe3.jsx
@@ -1,4 +1,4 @@
-export default function AboutMe3() {
+/*export default function AboutMe3() {
     return (
       <section id="AboutMe3" className="about--section2">
         <div className="about--section--img">
@@ -8,10 +8,30 @@ export default function AboutMe3() {
           <div className="hero--section--content">
             <h1 className="skills-section--heading">Projetos de Extensão</h1>
             <p className="hero--section-description">
-            O laboratório de projetos mecânicos desenvolve projetos de extensão com alunos de graduação e pós-graduação de todos os cursos da UFS, dentre as atividades estão a Zenith Rocketry, equipe de foguetes e cubesats criadas em 2022, e a oficina de minifoguetes e astronomia.             </p>
+            O laboratório de projetos mecânicos desenvolve projetos de extensão com alunos de graduação e pós-graduação de todos os cursos da UFS, dentre as atividades estão a Zenith Rocketry, equipe de foguetes e cubesats criadas em 2022, e a oficina de minifoguetes e astronomia.    
+                     </p>
 
           </div>
         </div>
       </section>
     );
   }
+  */
+
+  
+export default function AboutMe2() {
+  return (
+    <section id="AboutMe3" className="hero--section" style={{ backgroundColor: 'white' }}>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <h1 className="skills-section--heading">Projetos de Extensão</h1>
+          <p className="hero--section-description">
+          O laboratório de projetos mecânicos desenvolve projetos de extensão com alunos de graduação e pós-graduação de todos os cursos da UFS, dentre as atividades estão a Zenith Rocketry, equipe de foguetes e cubesats criadas em 2022, e a oficina de minifoguetes e astronomia.            </p>
+        </div>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/equipamentos.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
