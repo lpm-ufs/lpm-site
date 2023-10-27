af82fdc6f38f8a9611bfa67525996d29756aaa43
josedhonatas <dhonatas@academico.ufs.br>
2023-10-27 15:44:38 -0300

    quase

---
+++ src/Pages/Home/CustomCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+export default function CustomCard({ imageUrl, authorName, authorDesignation, description }) {
+  return (
+    <Card style={{ height: "100%", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)" }}>
+      <CardHeader
+        avatar={<Avatar src={imageUrl} alt="Avatar" sx={{ width: 60, height: 60 }} />}
+        title={<Typography variant="h6" color="black">{authorName}</Typography>}
+        subheader={<Typography variant="body2" color="textSecondary">{authorDesignation}</Typography>}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" style={{ maxHeight: "100px", overflow: "hidden", color: "black", fontSize: "1rem" }}>
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
--- src/Pages/Home/Testimonials.jsx
+++ src/Pages/Home/Testimonials.jsx
@@ -1,107 +1,54 @@
 import data from "../../data/index.json";
 
+import React, { useEffect, useState } from 'react';
+import CustomCard from "./CustomCard";
+import { Container, Grid, Box } from '@mui/material';
+
+
 export default function Testimonial() {
-  const studentCi = data?.student_ci || [];
-  const studentsInGroupsOfThree = [];
-
-  // Dividir os dados em grupos de três
-  for (let i = 0; i < studentCi.length; i += 3) {
-    studentsInGroupsOfThree.push(studentCi.slice(i, i + 3));
-  }
-
   return (
     <section className="testimonial--section" id="students">
       <div className="portfolio--container-box">
         <div className="portfolio--container">
-          <h2 className="sections--heading">Alunos de Iniciação Cientifica e Tecnológica</h2>
+          <h2 className="sections--heading">Alunos de Iniciação Científica e Tecnológica</h2>
         </div>
       </div>
-      {studentsInGroupsOfThree.map((group, groupIndex) => (
-        <div key={groupIndex} className="portfolio--section--container">
-          {group.map((item, index) => (
-            item.id == 10 ? <div key={index} className="testimonial--section--card2">
-              <div className="testimonial--section--card--author--detail">
-              </div>
-            </div> : (
-              <div key={index} className="testimonial--section--card">
-                <div className="testimonial--section--card--author--detail">
-                <img className="rounded-image" src={item.src} alt="Avatar" />
-                  <div>
-                    <p className="text-md testimonial--author--name">
-                      {item.author_name}
-                    </p>
-                    <p className="text-md testimonial--author--designation">
-                      {item.author_designation}
-                    </p>
-                  </div>
-                </div>
-                <p className="text-md">{item.description}</p>
-              </div>
-            )
-          ))}
-        </div>
-      ))}
-      <div className="portfolio--container-box">
-        <div className="portfolio--container">
-          <h2 className="sections--heading">Alunos de Trabalho de Conclusão de Curso</h2>
-        </div>
+      <div style={{}}>
+        <Container maxWidth={false}>
+          <Grid container spacing={3}>
+            {data?.student_ci?.map((anime) => (
+              <Grid item xs={12} sm={12} md={4} lg={4}>
+                <CustomCard imageUrl={anime.src} authorName={anime.author_name} authorDesignation={anime.author_designation} description={anime.description} />
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </div>
+      <h2 className="sections--heading">Alunos de Trabalho de Conclusão de Curso</h2>
+      <div style={{}}>
+        <Container maxWidth={false}>
+          <Grid container spacing={3}>
+            {data?.students_tcc?.map((anime) => (
+              <Grid item xs={12} sm={12} md={4} lg={4}>
+                <CustomCard imageUrl={anime.src} authorName={anime.author_name} authorDesignation={anime.author_designation} description={anime.description} />
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </div>
 
-      <div className="portfolio--section--container">
-        {data?.students_tcc?.map((item, index) => (
-          item.id == 10 ? <div key={index} className="testimonial--section--card2">
-            <div className="testimonial--section--card--author--detail">
-            </div>
-          </div> : (
-            <div key={index} className="testimonial--section--card">
-              <div className="testimonial--section--card--review"></div>
-              <div className="testimonial--section--card--author--detail">
-              <img className="rounded-image" src={item.src} alt="Avatar" />
-                <div>
-                  <p className="text-md testimonial--author--name">
-                    {item.author_name}
-                  </p>
-                  <p className="text-md testimonial--author--designation">
-                    {item.author_designation}
-                  </p>
-                </div>
-              </div>
-              <p className="text-md">{item.description}</p>
-            </div>
-          )
-        ))}
-      </div>
-
-      <div className="portfolio--container-box">
-        <div className="portfolio--container">
-          <h2 className="sections--heading">Alunos de Pós-graduação</h2>
-        </div>
-      </div>
-
-      <div className="portfolio--section--container">
-        {data?.students_pos?.map((item, index) => (
-          item.id === 10 ? <div key={index} className="testimonial--section--card2">
-            <div className="testimonial--section--card--review"></div>
-            <div className="testimonial--section--card--author--detail">
-            </div>
-          </div> : (
-            <div key={index} className="testimonial--section--card3">
-              <div className="testimonial--section--card--review"></div>
-              <div className="testimonial--section--card--author--detail">
-              <img className="rounded-image" src={item.src} alt="Avatar" />
-                <div>
-                  <p className="text-md testimonial--author--name">
-                    {item.author_name}
-                  </p>
-                  <p className="text-md testimonial--author--designation">
-                    {item.author_designation}
-                  </p>
-                </div>
-              </div>
-            </div>
-          )
-        ))}
+      <h2 className="sections--heading">Alunos de Trabalho de Conclusão de Curso</h2>
+      <div style={{}}>
+        <Container maxWidth={false}>
+          <Grid container spacing={3}>
+            {data?.students_pos?.map((anime) => (
+              <Grid item xs={12} sm={12} md={6} lg={6}>
+                <CustomCard imageUrl={anime.src} authorName={anime.author_name} authorDesignation={anime.author_designation} description={anime.description} />
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </div>
     </section>
   );
-}
+            }  
