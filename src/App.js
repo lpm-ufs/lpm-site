60ca6c6195b0df5f34fdf1f8ebe96542555da996
josedhonatas <dhonatas@academico.ufs.br>
2023-11-01 19:10:53 -0300

    config

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     <div className="App">
       <Router>
         <div>
-          {isSmallScreen ? <Navbar2 /> : <Navbar />}
+          {isSmallScreen ? <Navbar /> : <Navbar />}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="*" element={<div>404 Not Found</div>} />
