ac478a9de942865c8f5a50157e4349728cd28b40
josedhonatas <dhonatas@academico.ufs.br>
2023-11-02 12:42:51 -0300

    card_ui

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -8,30 +8,34 @@ export default function Testimonial() {
   const { t } = useTranslation();
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      bgcolor="white"
-    >
-                <h2 className="skills-section--heading">{t('Contato')}</h2>
-                <br></br>
+    <section className="" id="ContactMe">
 
-      <Container maxWidth="sm"> {/* Defina um valor adequado para o maxWidth */}
-        <Grid container justifyContent="center" alignItems="center">
-          <Grid item>
-            <CustomCard
-              imageUrl={data?.testimonial2[0]?.src}
-              authorName={data?.testimonial2[0]?.author_name}
-              authorDesignation={data?.testimonial2[0]?.email}
-              description={data?.testimonial2[0]?.description}
-            />
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        bgcolor="white"
+      >
+        <h2 className="skills-section--heading">{t('Contato')}</h2>
+        <br></br>
+
+        <Container maxWidth="sm"> {/* Defina um valor adequado para o maxWidth */}
+          <Grid container justifyContent="center" alignItems="center">
+            <Grid item>
+              <CustomCard
+                imageUrl={data?.testimonial2[0]?.src}
+                authorName={data?.testimonial2[0]?.author_name}
+                authorDesignation={data?.testimonial2[0]?.email}
+                description={data?.testimonial2[0]?.description}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
-      <br></br>
-      <br></br>
-    </Box>
+        </Container>
+        <br></br>
+        <br></br>
+      </Box>
+    </section>
+
   );
 }
--- src/Pages/Home/Testimonials.jsx
+++ src/Pages/Home/Testimonials.jsx
@@ -1,5 +1,5 @@
 import data from "../../data/index.json";
-import {useTranslation} from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 import React, { useEffect, useState } from 'react';
 import CustomCard from "./CustomCard";
@@ -7,51 +7,63 @@ import { Container, Grid, Box } from '@mui/material';
 
 
 export default function Testimonial() {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   return (
-    <section className="testimonial--section" id="students">
-      <div className="portfolio--container-box">
-        <div className="portfolio--container">
-          <h2 className="skills-section--heading">{t('t1_ic')}</h2>
+    <section className="" id="students">
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        bgcolor="#ffbe8f"
+      >
+        <br></br><br></br>
+        <h2 className="skills-section--heading">{t('t1_ic')}</h2>
+        <br></br><br></br>
+        <div style={{}}>
+          <Container maxWidth={false}>
+            <Grid container spacing={3}>
+              {data?.student_ci?.map((anime) => (
+                <Grid item xs={12} sm={12} md={4} lg={4}>
+                  <CustomCard imageUrl={anime.src} authorName={anime.author_name} authorDesignation={anime.author_designation} description={anime.description} />
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
         </div>
-      </div>
-      <div style={{}}>
-        <Container maxWidth={false}>
-          <Grid container spacing={3}>
-            {data?.student_ci?.map((anime) => (
-              <Grid item xs={12} sm={12} md={4} lg={4}>
-                <CustomCard imageUrl={anime.src} authorName={anime.author_name} authorDesignation={anime.author_designation} description={anime.description} />
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </div>
-      <h2 className="skills-section--heading">{t('t1_tcc')}</h2>
-      <div style={{}}>
-        <Container maxWidth={false}>
-          <Grid container spacing={3}>
-            {data?.students_tcc?.map((anime) => (
-              <Grid item xs={12} sm={12} md={4} lg={4}>
-                <CustomCard imageUrl={anime.src} authorName={anime.author_name} authorDesignation={anime.author_designation} description={anime.description} />
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </div>
+        <br></br><br></br>
+        <h2 className="skills-section--heading">{t('t1_tcc')}</h2>
+        <br></br><br></br>
+        <div style={{}}>
+          <Container maxWidth={false}>
+            <Grid container spacing={3}>
+              {data?.students_tcc?.map((anime) => (
+                <Grid item xs={12} sm={12} md={4} lg={4}>
+                  <CustomCard imageUrl={anime.src} authorName={anime.author_name} authorDesignation={anime.author_designation} description={anime.description} />
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </div>
+        <br></br><br></br>
+        <h2 className="skills-section--heading">{t('t1_pos')}</h2>
+        <br></br><br></br>
+        <div style={{}}>
+          <Container maxWidth={false}>
+            <Grid container spacing={3}>
+              {data?.students_pos?.map((anime) => (
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                  <CustomCard imageUrl={anime.src} authorName={anime.author_name} authorDesignation={anime.author_designation} description={anime.description} />
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </div>
+        <br></br><br></br>
 
-      <h2 className="skills-section--heading">{t('t1_pos')}</h2>
-      <div style={{}}>
-        <Container maxWidth={false}>
-          <Grid container spacing={3}>
-            {data?.students_pos?.map((anime) => (
-              <Grid item xs={12} sm={12} md={6} lg={6}>
-                <CustomCard imageUrl={anime.src} authorName={anime.author_name} authorDesignation={anime.author_designation} description={anime.description} />
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </div>
+      </Box>
+
     </section>
   );
-            }  
+}  
